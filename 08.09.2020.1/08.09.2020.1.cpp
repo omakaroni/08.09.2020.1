7bc1acac59e0f1d7b75bac8364759b280d1aa515
omakaroni <jon_resell_3@hotmail.com>
2020-09-08 16:48:46 +0200

    work work

--- 08.09.2020.1/08.09.2020.1.cpp
+++ 08.09.2020.1/08.09.2020.1.cpp
@@ -1,39 +1,128 @@
 #include <iostream>
+#include <string>
 
 int main() {
-	char option;
-	while (true) {
-		std::cout << "\tA. Save the game" << std::endl;
-		std::cout << "\tB. Restart the game" << std::endl;
-		std::cout << "\tC. Select difficulity" << std::endl;
-		std::cout << "\tF. Exit" << std::endl;
-		std::cout << "Select  An Option" << std::endl;
+	//	bool flag = true;
+	//	char option;
+	//	do {
+	//		std::cout << "\tA. Save the game" << std::endl;
+	//		std::cout << "\tB. Restart the game" << std::endl;
+	//		std::cout << "\tC. Select difficulity" << std::endl;
+	//		std::cout << "\tQ. Exit" << std::endl;
+	//		std::cout << "Select  An Option" << std::endl;
+	//		std::cout << std::endl;
+	//
+	//		std::cin >> option;
+	//
+	//		/*	if (option == 65 || option == 97) {
+	//				std::cout << "saving..." << std::endl;
+	//				std::cout << std::endl;
+	//			}
+	//			else if (option == 66 || option == 98) {
+	//				std::cout << "restarting the game" << std::endl;
+	//				std::cout << std::endl;
+	//			}
+	//			else if (option == 67 || option == 99) {
+	//				std::cout << "Please select difficulity (easy/hard)" << std::endl;
+	//				std::cout << std::endl;
+	//			}
+	//			else if (option == 70 || option == 102) {
+	//				exit(0);
+	//			}
+	//			else {
+	//				std::cout << "\tInvalid input" << std::endl;
+	//				std::cout << std::endl;
+	//			}*/
+	//		switch (option) {
+	//		case 'A': case 'a':
+	//			std::cout << "\tsaving..." << std::endl;
+	//			std::cout << std::endl;
+	//			break;
+	//		case 'B': case 'b':
+	//			std::cout << "\trestarting the game" << std::endl;
+	//			std::cout << std::endl;
+	//			break;
+	//		case 'C': case 'c':
+	//			std::cout << "\tPlease select difficulity (easy/hard)" << std::endl;
+	//			std::cout << std::endl;
+	//			break;
+	//		case 'Q': case 'q':
+	//			flag = false;
+	//			std::cout << "\tGoodbye!" << std::endl;
+	//			std::cout << std::endl;
+	//			break;
+	//		default:
+	//			std::cout << "\tInvalid input" << std::endl;
+	//			std::cout << std::endl;
+	//			break;
+	//		}
+	//	}
+	//	/*while (option != 'q' && option !='Q');
+	//	can use this instead of flags*/
+	//	while (flag == true);
+
+
+
+	// print even numbers between 0 to 20
+	//int i = 0;
+
+	//for (int i=0; i <= 20; i++) { // i++ i=i+1
+	//	if (i%2 == 0) {
+	//		std::cout <<" "<< i;
+	//	}
+	//}
+	//std::cout << std::endl;
+
+
+	//do {
+	//	if (i % 2 == 0) {
+	//		std::cout << " " << i;
+	//	}
+	//	i++;
+	//} while (i <= 20);
+	//std::cout << std::endl;
+	//
+
+	/*for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 4; j++) {
+			std::cout << "i = " << i << " ";
+			std::cout << "j = " << j << " ";
+		}
 		std::cout << std::endl;
+	}*/
 
-		std::cin >> option;
+	//int shirt;
+	//int suit;
+	//int pants;
+	//const int shirtP = 150;
+	//const int suitP = 100;
+	//const int pantsP = 200;
+	//const double tax = 0.25;
+	//double totalPrice = 0.0;
+	//double taxofTotal = 0.0;
+	//char answer;
 
-		if (option == 65 || option == 97) {
-			std::cout << "saving..." << std::endl;
-			std::cout << std::endl;
-		}
-		else if (option == 66 || option == 98) {
-			std::cout << "restarting the game" << std::endl;
-			std::cout << std::endl;
-		}
-		else if (option == 67 || option == 99) {
-			std::cout << "Please select difficulity (easy/hard)" << std::endl;
-			std::cout << std::endl;
-		}
-		else if (option == 70 || option == 102) {
-			exit(0);
-		}
-		else {
-			std::cout << "\tInvalid input" << std::endl;
-			std::cout << std::endl;
-		}
-	}
+	//do {
+	//	std::cout << "\t\tWelcome to the laudry" << std::endl;
+	//	std::cout << std::endl;
+	//	std::cout << "\thow many shirts do you have?" << std::endl;
+	//	std::cout << std::endl;
+	//	std::cin >> shirt;
+	//	std::cout << std::endl;
+	//	std::cout << "\thow many pants do you have?" << std::endl;
+	//	std::cout << std::endl;
+	//	std::cin >> pants;
+	//	std::cout << std::endl;
+	//	std::cout << "\thow many suits do you have?" << std::endl;
+	//	std::cout << std::endl;
+	//	std::cin >> suit;
+	//	std::cout << std::endl;
+	//	taxofTotal = (((shirt * shirtP) + (suit * suitP) + (pants * pantsP)) * tax);
+	//	totalPrice = (((shirt * shirtP) + (suit * suitP) + (pants * pantsP)) + taxofTotal);
+	//	std::cout << "\tYou bill comes to: " << totalPrice << std::endl;
+	//	std::cout << std::endl;
+	//	std::cout << "\tWill you like to conntinue?" << std::endl;
+	//	std::cin >> answer;
+	//} while (answer == 'y');
+	return 0;
 }
-
-
-
-
